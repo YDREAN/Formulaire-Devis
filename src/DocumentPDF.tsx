6e03f1e8cf115c10ea3f9ca4530cab20f5455266
YDREAN <yanndrean1@gmail.Com>
2024-06-16 01:00:06 +0200

    type acompte  créer et refactor devis à terminer

--- src/DocumentPDF.tsx
+++ src/DocumentPDF.tsx
@@ -33,6 +33,12 @@ type FooterValues = {
   totalTVA: number;
 };
 
+type Acomptes = {
+  percentage: number;
+  unit: string;
+  option: string;
+};
+
 const tw = createTw({
   theme: {
     fontFamily: {
@@ -52,52 +58,13 @@ export const DocumentPDF: React.FC<{
   tabValues: TabValue[];
   bankDetails: BankDetails;
   footerValues: FooterValues;
-  acomptes: { percentage: number; unit: string; option: string }[];
+  acomptes: Acomptes[];
 }> = ({ data, tabValues, bankDetails, footerValues, acomptes }) => (
   <Document>
     <Page style={tw("p-2 text-sans")} size="A4">
       <View style={tw("flex flex-row justify-between h-1/5")}>
-        <View>
-          <Image src="./src/assets/dcrrlogo.png" style={tw("w-44")} />
-          <Text style={tw("text-sm font-bold")}>
-            Denquin & Ciatti Refrigeration Reglementary
-          </Text>
-          <Text style={tw("text-sm")}>
-            36 Avenue Adrien Raynal, 94310 - ORLY
-          </Text>
-          <Text style={tw("text-sm")}>SAS au capital de 1000€</Text>
-          <Link src="https://dcrr.fr/contact/" style={tw("text-sm")}>
-            Contactez-nous
-          </Link>
-          <View>
-            <Text style={tw("text-sm")}>
-              <Text style={tw("font-bold")}>RCS </Text>
-              Créteil B 951 054 394
-            </Text>
-            <Text style={tw("text-sm")}>
-              <Text style={tw("font-bold")}>APE </Text>
-              7120B
-            </Text>
-            <Text style={tw("text-sm")}>
-              <Text style={tw("font-bold text-red")}>SIRET </Text>
-              95105439400014
-            </Text>
-            <Text style={tw("text-sm")}>
-              <Text style={tw("font-bold ")}>TVA </Text>
-              FR53951054394
-            </Text>
-          </View>
-        </View>
-        <View style={tw("flex flex-col")}>
-          <Text style={tw("text-2xl self-end")}>Devis</Text>
-          <Text style={tw("inline self-end text-sm")}>
-            Objet du devis : {data.objetDevis}
-          </Text>
-          <Text style={tw("self-end text-sm")}>
-            Nom de l'affaire : {data.nomAffaire}
-          </Text>
-          <Text style={tw("self-end text-sm")}>Client : {data.client}</Text>
-        </View>
+        {DCRRInfo()}
+        {DevisHeadInfo(data)}
       </View>
       <View style={tw("mt-5")}>
         <View
@@ -133,54 +100,113 @@ export const DocumentPDF: React.FC<{
         ))}
       </View>
 
-      <View style={tw("flex flex-row justify-between mx-4")}>
-        <View>
-          <View style={tw("mt-5 border border-slate-300 p-2 rounded-lg")}>
-            <Text style={tw("text-2lg mb-2")}>Acompte</Text>
-            <View style={tw("flex ")}>
-              {acomptes.length > 0 ? (
-                acomptes.map((acompte, index) => (
-                  <Text key={index} style={tw("text-sm pl-2")}>
-                    {acompte.percentage} {acompte.unit} {acompte.option}
-                  </Text>
-                ))
-              ) : (
-                <Text style={tw("text-sm pl-2")}>Pas d'acompte</Text>
-              )}
-            </View>
-          </View>
-          <View style={tw("mt-5 border border-slate-300 p-2 rounded-lg")}>
-            <Text style={tw("text-2lg mb-2")}>Détails de paiement</Text>
-            <View style={tw("flex flex-row")}>
-              <Image src="./src/assets/banque.png" style={tw("w-5")} />
-              <Text style={tw("text-sm pl-2")}>IBAN : {bankDetails.iban}</Text>
-            </View>
-            <View style={tw("flex flex-row")}>
-              <Image src="./src/assets/cheque-bancaire.png" style={tw("w-5")} />
-              <Text style={tw("text-sm pl-2")}>
-                Nom de l'entreprise : {bankDetails.companyName}
-              </Text>
-            </View>
-          </View>
-        </View>
-
-        <View style={tw("mt-5 border border-slate-300 rounded-lg")}>
-          <Text style={tw("text-2xl mb-2 p-2")}>Résumé</Text>
-          <Text style={tw("text-lg p-2")}>
-            Total HT: {footerValues.totalHT} €
-          </Text>
-          <Text style={tw("text-lg p-2")}>
-            Total TVA: {footerValues.totalTVA.toFixed(2)} €
-          </Text>
-          <Text
-            style={tw(
-              "text-lg rounded-b-lg bg-lime-600 pt-2 px-4 text-xl text-white font-bold"
-            )}
-          >
-            Net à payer: {footerValues.net.toFixed(2)} €
-          </Text>
-        </View>
-      </View>
+      {DevisFooter(acomptes, bankDetails, footerValues)}
     </Page>
   </Document>
 );
+
+const DevisFooter = (
+  acomptes: { percentage: number; unit: string; option: string }[],
+  bankDetails: BankDetails,
+  footerValues: FooterValues
+) => {
+  return (
+    <View style={tw("bg-yellow-200 flex flex-row justify-between mx-4")}>
+      <View style={tw("bg-red-100")}>
+        <View style={tw("mt-5 border border-slate-300 p-2 rounded-lg")}>
+          <Text style={tw("text-2lg mb-2")}>Acompte</Text>
+          <View style={tw("flex ")}>
+            {acomptes.length > 0 ? (
+              acomptes.map((acompte, index) => (
+                <Text key={index} style={tw("text-sm pl-2")}>
+                  {acompte.percentage} {acompte.unit} {acompte.option}
+                </Text>
+              ))
+            ) : (
+              <Text style={tw("text-sm pl-2")}>Pas d'acompte</Text>
+            )}
+          </View>
+        </View>
+        <View style={tw("mt-5 border border-slate-300 p-2 rounded-lg")}>
+          <Text style={tw("text-2lg mb-2")}>Détails de paiement</Text>
+          <View style={tw("flex flex-row")}>
+            <Image src="./src/assets/banque.png" style={tw("w-5")} />
+            <Text style={tw("text-sm pl-2")}>IBAN : {bankDetails.iban}</Text>
+          </View>
+          <View style={tw("flex flex-row")}>
+            <Image src="./src/assets/cheque-bancaire.png" style={tw("w-5")} />
+            <Text style={tw("text-sm pl-2")}>
+              Nom de l'entreprise : {bankDetails.companyName}
+            </Text>
+          </View>
+        </View>
+      </View>
+
+      <View style={tw("mt-5 border border-slate-300 rounded-lg")}>
+        <Text style={tw("text-2xl mb-2 p-2")}>Résumé</Text>
+        <Text style={tw("text-lg p-2")}>
+          Total HT: {footerValues.totalHT} €
+        </Text>
+        <Text style={tw("text-lg p-2")}>
+          Total TVA: {footerValues.totalTVA.toFixed(2)} €
+        </Text>
+        <Text
+          style={tw(
+            "text-lg rounded-b-lg bg-lime-600 pt-2 px-4 text-xl text-white font-bold"
+          )}
+        >
+          Net à payer: {footerValues.net.toFixed(2)} €
+        </Text>
+      </View>
+    </View>
+  );
+};
+
+const DevisHeadInfo = (data: DevisSchema) => {
+  return (
+    <View style={tw("bg-blue-100 flex flex-col")}>
+      <Text style={tw("text-2xl self-end")}>Devis</Text>
+      <Text style={tw("inline self-end text-sm")}>
+        Objet du devis : {data.objetDevis}
+      </Text>
+      <Text style={tw("self-end text-sm")}>
+        Nom de l'affaire : {data.nomAffaire}
+      </Text>
+      <Text style={tw("self-end text-sm")}>Client : {data.client}</Text>
+    </View>
+  );
+};
+
+const DCRRInfo = () => {
+  return (
+    <View style={tw("bg-red-200")}>
+      <Image src="./src/assets/dcrrlogo.png" style={tw("w-44")} />
+      <Text style={tw("text-sm font-bold")}>
+        Denquin & Ciatti Refrigeration Reglementary
+      </Text>
+      <Text style={tw("text-sm")}>36 Avenue Adrien Raynal, 94310 - ORLY</Text>
+      <Text style={tw("text-sm")}>SAS au capital de 1000€</Text>
+      <Link src="https://dcrr.fr/contact/" style={tw("text-sm")}>
+        Contactez-nous
+      </Link>
+      <View>
+        <Text style={tw("text-sm")}>
+          <Text style={tw("font-bold")}>RCS </Text>
+          Créteil B 951 054 394
+        </Text>
+        <Text style={tw("text-sm")}>
+          <Text style={tw("font-bold")}>APE </Text>
+          7120B
+        </Text>
+        <Text style={tw("text-sm")}>
+          <Text style={tw("font-bold text-red")}>SIRET </Text>
+          95105439400014
+        </Text>
+        <Text style={tw("text-sm")}>
+          <Text style={tw("font-bold ")}>TVA </Text>
+          FR53951054394
+        </Text>
+      </View>
+    </View>
+  );
+};
