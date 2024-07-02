d883902d557ebb1abc87b15d8e9e0a7dc4784b1f
YDREAN <yanndrean1@gmail.Com>
2024-07-02 03:12:35 +0200

    app import fix

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import FormView from "./Acceuil";
 import Acceuil from "./Acceuil";
 import Menu from "./Acceuil";
 import FormDevis from "./FormDevis";
-import { PlanInspection } from "./PlanInspection";
+import PlanInspection from "./PlanInspection";
 
 function App() {
   return (
